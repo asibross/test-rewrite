246fab654be9b6e97341b972f129b95d173dd4c9
Asi Bross <11426802+asibross@users.noreply.github.com>
2024-11-18 11:16:10 -0800

    First commit!

---
+++ src/main/java/com/example/test_rewrite/TestRewriteApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_rewrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRewriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRewriteApplication.class, args);
+	}
+
+}
